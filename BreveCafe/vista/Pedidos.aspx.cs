de57fca3752a9687f20ff241ab1862dedca1288a
Sergio241018 <sergioajimenez2410@gmail.com>
2025-02-03 15:32:39 -0500

    funcion de aumentar y disminuir cantidad de productos en carrito y obtener mesas desde la bd

--- BreveCafe/vista/Pedidos.aspx.cs
+++ BreveCafe/vista/Pedidos.aspx.cs
@@ -38,14 +38,41 @@ namespace BreveCafe.vista
             }
         }
 
+        protected void gvCarrito_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                DropDownList ddlMesa = (DropDownList)e.Row.FindControl("ddlMesa");
+                if (ddlMesa != null)
+                {
+                    var mesas = carritoService.ObtenerMesas();
+                    ddlMesa.DataSource = mesas;
+                    ddlMesa.DataBind();
+
+                    // Aquí podrías establecer un valor seleccionado si es necesario
+                    // Ejemplo: ddlMesa.SelectedValue = producto.MesaId; si tienes el ID de la mesa asociada
+                }
+            }
+        }
+
         protected void gvCarrito_RowCommand(object sender, GridViewCommandEventArgs e)
         {
+            int idProducto = Convert.ToInt32(e.CommandArgument);
+
             if (e.CommandName == "Eliminar")
             {
-                int idProducto = Convert.ToInt32(e.CommandArgument);
                 carritoService.EliminarProductoDelCarrito(idUsuario, idProducto);
-                CargarCarrito();
             }
+            else if (e.CommandName == "Mas")
+            {
+                carritoService.AjustarCantidadProducto(idUsuario, idProducto, 1);
+            }
+            else if (e.CommandName == "Menos")
+            {
+                carritoService.AjustarCantidadProducto(idUsuario, idProducto, -1);
+            }
+
+            CargarCarrito();
         }
 
         protected void btnProcederPago_Click(object sender, EventArgs e)
@@ -54,4 +81,3 @@ namespace BreveCafe.vista
         }
     }
 }
-
---
+++ BreveCafe/vista/VerPedidos.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/vista/MaestraAdministrador.Master" AutoEventWireup="true" CodeBehind="VerPedidos.aspx.cs" Inherits="BreveCafe.vista.Formulario_web1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="Contenthead" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentBody" runat="server">
+   <h2 class="mt-5">hola</h2>
+</asp:Content>
